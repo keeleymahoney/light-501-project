3ed7b3eb876485cac3e5d23fa7a2598d87e2a1d0
Keeley Mahoney <keeleymahoney@Keeleys-MacBook-Air.local>
2024-09-17 22:22:56 -0500

    added in basic ui for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,6 @@
 class Event < ApplicationRecord
     validates :name, presence: true
-    validates: date, presence: true
-    validates: description, presence: true
-    validates: location, presence: true
-    validates: attendees, presence: true
+    validates :date, presence: true
+    validates :description, presence: true
+    validates :location, presence: true
 end
