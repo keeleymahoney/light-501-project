87b0ce734bf5e944ed12733dcbcaa37b40d40989
adrianc-17 <adriancor@tamu.edu>
2024-09-22 00:06:23 +0000

    deleted previous controller/view and generated more fitting controller/view for forms

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,10 @@
+class FormsController < ApplicationController
+  def create
+  end
+
+  def monitor
+  end
+
+  def respond
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'main#index'
+  root 'forms#create'
 
-  get 'main/index'
+  get 'forms/create'
+  get 'forms/monitor'
+  get 'forms/respond'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
