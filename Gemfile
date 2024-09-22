8a36b26b44f42fa8c0ae627e779d1f0193bf5fe9
adrianc-17 <adriancor@tamu.edu>
2024-09-22 18:30:54 +0000

    got google drive api working. Got basic form functionality to share permissions and delete

--- Gemfile
+++ Gemfile
@@ -72,3 +72,4 @@ group :test do
 end
 
 gem 'google-apis-forms_v1', '~> 0.3.0'
+gem 'google-apis-drive_v3', '~> 0.5.0' 
--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,20 +1,35 @@
 class FormsController < ApplicationController
   require 'google/apis/forms_v1'
+  require 'google/apis/drive_v3'
 
   def create
     forms = Google::Apis::FormsV1::FormsService.new
+    drive = Google::Apis::DriveV3::DriveService.new
 
-    scopes = ['https://www.googleapis.com/auth/forms.body']
-    forms.authorization = Google::Auth.get_application_default(scopes)
+    form_scopes = ['https://www.googleapis.com/auth/forms.body']
+    forms.authorization = Google::Auth.get_application_default(form_scopes)
+
+    drive_scopes = ['https://www.googleapis.com/auth/drive']
+    drive.authorization = Google::Auth.get_application_default(drive_scopes)    
     
-    @new_form = forms.create_form(
-      {
-        "info": {
-          "title": "Test Title",
-          "documentTitle": "Test Document Title",
-        }
-      }      
-    )
+    # @new_form = forms.create_form(
+    #   {
+    #     "info": {
+    #       "title": "Test Create Form",
+    #       "documentTitle": "Test Create Form",
+    #     }
+    #   }      
+    # )
+    
+    # @form_permissions = drive.create_permission(fileId=@new_form.form_id,{
+    #   'email_address': 'test4light2day@gmail.com',
+    #   'type': 'user',
+    #   'role': 'writer'
+    # })
+
+    # drive.delete_file(fileId='10laRvqXr8kVWv4-hDd-DbWwIsO-UI5AxbBJOxFeGRJg')
+
+    @drive_list = drive.list_files()
     
   end
 
@@ -24,7 +39,7 @@ class FormsController < ApplicationController
     scopes = ['https://www.googleapis.com/auth/forms.responses.readonly']
     forms.authorization = Google::Auth.get_application_default(scopes)
 
-    @rsvp_form = forms.list_form_responses(formId='1RJfBCqaPYto6HBo-Z_BzHSGF-q-cLoROwMM3MhsvXt8') # Remove once Event entity available
+    @rsvp_form = forms.list_form_responses(formId='1IS7NqkaJjabeqtH_zD5diJfKwP62Zcb_9F5bTiIoWtI') # Remove once Event entity available
     # @curr_form = forms.list_form_responses(formId=rsvp_form_id)
     @rsvp_responses = 0
 
@@ -39,7 +54,7 @@ class FormsController < ApplicationController
     scopes = ['https://www.googleapis.com/auth/forms.body.readonly']
     forms.authorization = Google::Auth.get_application_default(scopes)
 
-    @rsvp_form = forms.get_form(formId='1RJfBCqaPYto6HBo-Z_BzHSGF-q-cLoROwMM3MhsvXt8') # Remove once Event entity available
+    @rsvp_form = forms.get_form(formId='1IS7NqkaJjabeqtH_zD5diJfKwP62Zcb_9F5bTiIoWtI') # Remove once Event entity available
     @feedback_form = forms.get_form(formId='1PlCUYNTYhGXwLDQGJLbgedW4abpZoq1wpjWVW_bemjI') # Remove once Event entity available
   end
 
