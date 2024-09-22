b525cc41edb7e1aff34643335310751f565639ed
adrianc-17 <adriancor@tamu.edu>
2024-09-22 05:47:20 +0000

    created basic page to monitor rsvp and feedback surveys

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,22 +1,33 @@
 class FormsController < ApplicationController
+  require 'google/apis/forms_v1'
 
   def create
   end
 
   def monitor
-    require 'google/apis/forms_v1'
-
     forms = Google::Apis::FormsV1::FormsService.new
 
     scopes = ['https://www.googleapis.com/auth/forms.responses.readonly']
     forms.authorization = Google::Auth.get_application_default(scopes)
 
-    # @curr_form = forms.get_form(formId='1RJfBCqaPYto6HBo-Z_BzHSGF-q-cLoROwMM3MhsvXt8')
-    @curr_form = forms.list_form_responses(formId='1RJfBCqaPYto6HBo-Z_BzHSGF-q-cLoROwMM3MhsvXt8')
-    # @rsvp_link = Event.find(params[:id]).rsvp_link
-    # @rsvp_link = ""
+    @rsvp_form = forms.list_form_responses(formId='1RJfBCqaPYto6HBo-Z_BzHSGF-q-cLoROwMM3MhsvXt8') # Remove once Event entity available
+    # @curr_form = forms.list_form_responses(formId=rsvp_form_id)
+    @rsvp_responses = 0
+
+    @feedback_form = forms.list_form_responses(formId='1PlCUYNTYhGXwLDQGJLbgedW4abpZoq1wpjWVW_bemjI') # Remove once Event entity available
+    # @curr_form = forms.list_form_responses(formId=feedback_form_id)
+    @feedback_responses = 0    
   end
 
   def respond
   end
+
+  private
+    def rsvp_form_id
+      # Event.find(params[:id]).rsvp_form_id
+    end
+
+    def feedback_form_id
+      # Event.find(params[:id]).feedback_form_id
+    end
 end
