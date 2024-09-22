e1b8d601184fec82ff8ba72c5df894c5cef6bfc7
adrianc-17 <adriancor@tamu.edu>
2024-09-22 02:10:48 +0000

    google api confirmed working

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,8 +1,20 @@
 class FormsController < ApplicationController
+
   def create
   end
 
   def monitor
+    require 'google/apis/forms_v1'
+
+    forms = Google::Apis::FormsV1::FormsService.new
+
+    scopes = ['https://www.googleapis.com/auth/forms.responses.readonly']
+    forms.authorization = Google::Auth.get_application_default(scopes)
+
+    # @curr_form = forms.get_form(formId='1RJfBCqaPYto6HBo-Z_BzHSGF-q-cLoROwMM3MhsvXt8')
+    @curr_form = forms.list_form_responses(formId='1RJfBCqaPYto6HBo-Z_BzHSGF-q-cLoROwMM3MhsvXt8')
+    # @rsvp_link = Event.find(params[:id]).rsvp_link
+    # @rsvp_link = ""
   end
 
   def respond
