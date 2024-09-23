da2a2bf5e7f77aa28ef8a83f9d3e7b713c80bcaa
Keeley Mahoney <keeleymahoney@Keeleys-MacBook-Air.local>
2024-09-23 14:53:03 -0500

    further spec stuff

---
+++ spec/factory/event_images.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+    factory :event_image do
+      association :event 
+      picture { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/files/BOLD_Logo.jpg'), 'image/jpeg') }
+    end
+  end
