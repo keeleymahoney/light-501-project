dd8395777bb5ced11efc2a8e2699c25f85aff049
Ryan Parmer <ryan.parmer.22@gmail.com>
2024-09-23 06:50:02 -0500

    crud for contacts with delete bug

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,52 @@
+class ContactsController < ApplicationController
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to @contact, notice: 'Contact was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      redirect_to @contact, notice: 'Contact was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    if @contact.destroy
+      redirect_to contacts_url, notice: 'Contact was successfully destroyed.'
+    else
+
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :organization, :title, :link, :bio, :email)
+  end
+
+end
---
+++ db/migrate/20240923084927_create_contacts.rb
@@ -0,0 +1,15 @@
+class CreateContacts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :organization
+      t.string :title
+      t.string :link
+      t.text :bio
+
+      t.timestamps
+    end
+  end
+end
