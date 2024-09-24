8a8e585fc1edfc92cda56f80d318fad5507c1f1b
adrianc-17 <adriancor@tamu.edu>
2024-09-24 05:21:16 +0000

    exchanged env variables for rails creds

--- config/initializers/generate_google_credentials.rb
+++ config/initializers/generate_google_credentials.rb
@@ -5,19 +5,19 @@
 require 'dotenv/load'
 require 'json'
 
-private_key = ENV['GOOGLE_PRIVATE_KEY'].gsub('\\n', "\n")
+private_key = Rails.application.credentials.service_credentials[:google_private_key].gsub('\\n', "\n")
 
 credentials = {
   type: 'service_account',
-  project_id: ENV['GOOGLE_PROJECT_ID'],
-  private_key_id: ENV['GOOGLE_PRIVATE_KEY_ID'].gsub('\\n', "\n"),
+  project_id: Rails.application.credentials.service_credentials[:google_project_id],
+  private_key_id: Rails.application.credentials.service_credentials[:google_private_key_id],
   private_key: private_key,
-  client_email: ENV['GOOGLE_CLIENT_EMAIL'],
-  client_id: ENV['GOOGLE_CLIENT_ID'],
+  client_email: Rails.application.credentials.service_credentials[:google_client_email],
+  client_id: Rails.application.credentials.service_credentials[:google_client_id],
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url: ENV['GOOGLE_CLIENT_X509_CERT_URL']
+  client_x509_cert_url: Rails.application.credentials.service_credentials[:google_client_x509_cert_url]
 }
 
 File.write(Rails.root.join('config', 'google_credentials.json'), credentials.to_json)
