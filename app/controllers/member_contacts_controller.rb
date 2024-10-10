4dfa732731e4b877fa5c2677f74d5800e1b34d96
Keeley Mahoney <keeleymahoney@Keeleys-MacBook-Air.local>
2024-10-10 16:50:08 -0500

    added in stuff for member controller protection

--- app/controllers/member_contacts_controller.rb
+++ app/controllers/member_contacts_controller.rb
@@ -2,6 +2,13 @@
 
 class MemberContactsController < ApplicationController
   before_action :authenticate_member!
+
+  def authenticate_member!
+    unless member_signed_in?
+      redirect_to events_path, alert: "You need to sign in or sign up before continuing."
+    end
+  end
+
     def index
       @contacts = Contact.all
     end
--- app/controllers/member_networks_controller.rb
+++ app/controllers/member_networks_controller.rb
@@ -1,6 +1,12 @@
 class MemberNetworksController < ApplicationController
     before_action :authenticate_member!
 
+    def authenticate_member!
+        unless member_signed_in?
+          redirect_to events_path, alert: "You need to sign in or sign up before continuing."
+        end
+      end
+
     def index
         if current_member.present?
             email = current_member.email
