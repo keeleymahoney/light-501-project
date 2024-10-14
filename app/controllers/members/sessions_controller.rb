3efa8e448a7a8ad6d5815dfce203bc443d53aa98
Ryan Parmer <ryan.parmer.22@gmail.com>
2024-10-14 16:58:37 -0500

    weird oauth changes

--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -2,11 +2,11 @@
 class Members::SessionsController < Devise::SessionsController
   # Redirect to events page after signing out
   def after_sign_out_path_for(_resource_or_scope)
-    new_member_session_path # Redirect to sign in page
+    new_member_session_path  # Redirect to sign in page
   end
 
   # Redirect to the events index page after signing in
   def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || root_path # Redirect to events page
+    stored_location_for(resource_or_scope) || root_path  # Redirect to events page
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,6 @@ class Member < ApplicationRecord
   has_one :token
 
   def self.from_google(email:, full_name:, uid:, avatar_url:)
-    create_with(uid:, full_name:, avatar_url:).find_or_create_by!(email:)
+    create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
 end
